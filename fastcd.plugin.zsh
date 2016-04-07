a22c761b5303f8a0cc13a766f0a94bd07460f123
Egor Kovetskiy <e.kovetskiy@office.ngs.ru>
2016-04-07 11:42:51 +0600

    rewrote completion

--- fastcd.plugin.zsh
+++ fastcd.plugin.zsh
@@ -3,11 +3,7 @@ fastcd() {
     local maxdepth=$2
     local target=$3
 
-    local destination=$(
-        find "$directory" -maxdepth $maxdepth -name "$target" \
-            \( -type d -o -type l \) -print -quit
-    )
-
-    cd $destination
+    cd $(find "$(eval echo "$directory")" -maxdepth $maxdepth -name "$target" \
+        \( -type d -o -type l \) -print -quit)
 }
 
