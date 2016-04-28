111fdda16a3124b1d0900d2f766b746f76135378
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-04-28 13:00:33 +0600

    if no args passed, cd to base dir

--- fastcd.plugin.zsh
+++ fastcd.plugin.zsh
@@ -2,8 +2,22 @@ fastcd() {
     local directory=$1
     local maxdepth=$2
     local target=$3
+    local chosen_directory
 
-    cd $(find "$(eval echo "$directory")" -maxdepth $maxdepth -name "$target" \
-        \( -type d -o -type l \) -print -quit)
+    chosen_directory=$(find \
+        "$(eval echo "$directory")" \
+        -maxdepth $maxdepth \
+        -name "$target" \( \
+            -type d -o -type l \
+        \) \
+        -print \
+        -quit \
+    )
+
+    if [ "$chosen_directory" ]; then
+        cd "$chosen_directory"
+    else
+        cd $directory
+    fi
 }
 
