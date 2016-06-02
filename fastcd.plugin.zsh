b0ae4b4759ede6699731206851500e8e18a2bebc
Stanislav Seletskiy <s.seletskiy@gmail.com>
2016-06-02 16:00:19 +0600

    fuzzy match, fourth param to control it

--- fastcd.plugin.zsh
+++ fastcd.plugin.zsh
@@ -2,12 +2,25 @@ fastcd() {
     local directory=$1
     local maxdepth=$2
     local target=$3
+    local fuzzy="false"
+
+    if [ $# -gt 3 ]; then
+        local fuzzy="$3"
+        local target="$4"
+    fi
+
     local chosen_directory
+
+    local predicate=(-name "$target")
+    if $fuzzy; then
+        local predicate=(-iname "*${(j:*:)${(s::)target}}*")
+    fi
 
     chosen_directory=$(find \
         "$(eval echo "$directory")" \
         -maxdepth $maxdepth \
-        -name "$target" \( \
+        "${predicate[@]}" \
+        \( \
             -type d -o -type l \
         \) \
         -print \
@@ -20,4 +33,3 @@ fastcd() {
         cd $directory
     fi
 }
-
